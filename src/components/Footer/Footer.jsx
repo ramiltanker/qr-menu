f3627ac5757c5b54c9d4cbdbfdb055b1181154ba
Ramil Ashrafulin <HEHAHA@bk.ru>
2021-10-05 16:49:45 +0600

    fix: animation, footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -5,7 +5,15 @@ import styles from './Footer.module.css';
 // Styles
 
 const Footer = () => {
-  return <footer className={styles.footer}></footer>;
+  return (
+    <footer className={styles.footer}>
+      <p className={styles.text}>
+        <span>Tino</span> Pizza
+      </p>
+      <p className={styles.text}>🍕Пицца на дровах🍕</p>
+      <p className={styles.text}>График работы: 10:00-00:00</p>
+    </footer>
+  );
 };
 
 export default Footer;
